769fe5a3f3cc9ec126b6a172b7aa311a296c9b38
Ahmad Fauzan <gavier.exe@gmail.com>
2025-08-12 10:32:00 +0700

    Statusline & Tabline Improvement

--- lua/core/ui/statusline.lua
+++ lua/core/ui/statusline.lua
@@ -1,58 +1,61 @@
-local function get_mode_strig()
+local function get_mode_name()
   local mode        = vim.fn.mode()
-  local mode_string = {
-    n       = 'NRML',
-    i       = 'INST',
-    v       = 'VSAL',
-    V       = 'VLNE',
-    ['\22'] = 'VBLK',
-    s       = 'SLCT',
-    S       = 'SLNE',
-    ['\19'] = 'SBLK',
-    c       = 'CMND',
-    R       = 'RPLC',
-    r       = 'RPLC',
-    ['!']   = 'SHLL',
-    t       = 'TRML'
+  local mode_name = {
+    n       = ' NRML ',
+    i       = ' INST ',
+    v       = ' VSAL ',
+    V       = ' VLNE ',
+    ['\22'] = ' VBLK ',
+    s       = ' SLCT ',
+    S       = ' SLNE ',
+    ['\19'] = ' SBLK ',
+    c       = ' CMND ',
+    R       = ' RPLC ',
+    r       = ' RPLC ',
+    ['!']   = ' SHLL ',
+    t       = ' TRML '
   }
 
-  return mode_string[mode] or '????'
+  return mode_name[mode] or '????'
 end
 
 local function get_file_name()
-  local file_name = ''
-
+  local file_name  = ''
   if vim.bo.filetype == 'explorer' then
     file_name = 'Explorer'
   elseif vim.bo.filetype == 'picker' then
     file_name = 'Picker'
-  elseif vim.bo.buftype == 'terminal' then
+  elseif vim.bo.filetype == 'terminal' then
     file_name = 'Terminal'
   else
     file_name = vim.fn.expand('%:t')
-    file_name = (file_name ~= '' and file_name) or 'No Name'
   end
+
+  file_name = file_name ~= '' and file_name or 'No Name'
+  file_name = ' ' .. file_name .. ' '
+  file_name = file_name
 
   return file_name
 end
 
 function _G.setup_statusline()
+  local mode_name       = get_mode_name()
   local file_name       = get_file_name()
-  local mode_string     = get_mode_strig()
   local cursor_position = '%l:%c:%p%%'
+  local separator       = '│'
 
   local format = {
     '%#StatusLine#',
     ' ',
-    mode_string,
+    mode_name,
     ' ',
-    '|',
+    separator,
     ' ',
     file_name,
     ' ',
-    '|',
+    separator,
     '%=',
-    '|',
+    separator,
     ' ',
     cursor_position,
     ' ',
--- lua/core/ui/tabline.lua
+++ lua/core/ui/tabline.lua
@@ -1,54 +1,63 @@
-local function get_file_name()
-  local file_name = ''
+local function get_version()
+  local version = string.format('Neovim %s.%s.%s',
+                                vim.version().major,
+                                vim.version().minor,
+                                vim.version().patch)
 
+  return version
+end
+
+local function get_file_name()
+  local file_name  = ''
+  local file_flags = '%h%m%r'
   if vim.bo.filetype == 'explorer' then
     file_name = 'Explorer'
+    file_flags = '*'
   elseif vim.bo.filetype == 'picker' then
     file_name = 'Picker'
-  elseif vim.bo.buftype == 'terminal' then
+    file_flags = '*'
+  elseif vim.bo.filetype == 'terminal' then
     file_name = 'Terminal'
+    file_flags = '*'
   else
-    file_name = vim.fn.expand('%:p')
-    if file_name == '' or vim.fn.isdirectory(file_name) == 1 then
-      file_name = 'No Name'
-    end
+    file_name = vim.fn.expand('%:.\'')
   end
+
+  file_name = file_name ~= '' and file_name or 'No Name'
+  file_name = ' ' .. file_name .. ' '
+  file_name = file_name .. file_flags
 
   return file_name
 end
 
-function _G.setup_tabline()
-  local statusline_hl = vim.api.nvim_get_hl(0, {name = 'StatusLine'})
-  local banner     = 'Neovim!'
-  local file_name  = get_file_name()
-  local file_flags = '%h%m%r'
-  local tabs       = ''
-
-  file_name = (file_name ~= '' and file_name) or 'No Name'
-
+local function get_tabs_number()
+  local tab_number = ''
   for i = 1, vim.fn.tabpagenr('$') do
-    local tab_hl = i == vim.fn.tabpagenr() and '%#TabLineSel#' or '%#TabLine#'
-    tabs = tabs .. tab_hl .. i .. ' %T'
+    local tabline_hl = i == vim.fn.tabpagenr() and '%#TabLineSel#' or '%#TabLine#'
+    tab_number = tab_number .. tabline_hl .. ' ' .. i .. ' ' .. '%#TabLineSel#' .. '%#TabLine#'
   end
 
-  vim.api.nvim_set_hl(0, 'TabLine', statusline_hl)
-  vim.api.nvim_set_hl(0, 'TabLineFill', statusline_hl)
-  vim.api.nvim_set_hl(0, 'TabLineSel', {fg = statusline_hl.fg, bg = statusline_hl.bg, bold = true})
+  return tab_number
+end
+
+function _G.setup_tabline()
+  local version     = get_version()
+  local file_name   = get_file_name()
+  local tabs_number = get_tabs_number()
+  local separator   = '│'
 
   local format = {
     '%#TabLine#',
     ' ',
-    banner,
+    version,
     ' ',
-    '|',
+    separator,
     '%=',
     file_name,
-    ' ',
-    file_flags,
     '%=',
-    '|',
+    separator,
     ' ',
-    tabs,
+    tabs_number,
     ' ',
     '%#TabLine#'
   }
